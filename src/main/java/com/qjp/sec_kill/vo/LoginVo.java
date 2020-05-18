05c159b6707e38837565fbdadaff30df1060fe56
Thinkpad <1475477624@qq.com>
2020-05-18 20:21:31 +0800

    go on...

---
+++ src/main/java/com/qjp/sec_kill/vo/LoginVo.java
@@ -0,0 +1,30 @@
+package com.qjp.sec_kill.vo;
+
+/**
+ * description: LoginVo
+ * date: 2020/5/18 20:18
+ * author: 雨夜微凉
+ * version: 1.0
+ */
+public class LoginVo {
+    private String mobile;
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String password;
+
+}
